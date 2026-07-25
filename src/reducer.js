7ef9f3ec3fe4114e4345b32105f87fe8c9ff54fa
منور <125405236+monawdev@users.noreply.github.com>
2026-07-25 12:54:35 -0400

    Configure App component with useReducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,32 @@
 export const initialState = [];
-export function todoReducer(state, action) {
 
+export function todoReducer(state, action) {
+  switch (action.type) {
+
+    case "ADD":
+      return [
+        action.payload,
+        ...state
+      ];
+
+    case "DELETE":
+      return state.filter(todo => todo.id !== action.payload);
+
+    case "TOGGLE":
+      return state.map(todo =>
+        todo.id === action.payload
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      );
+
+    case "EDIT":
+      return state.map(todo =>
+        todo.id === action.payload.id
+          ? { ...todo, title: action.payload.title }
+          : todo
+      );
+
+    default:
+      return state;
+  }
 }
